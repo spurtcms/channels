2185ac62356d0adbcf97ac5d1066cbfba3c95f3c
spurtcmsdeveloper-146 <119719731+parvesh-piccosoft@users.noreply.github.com>
2024-05-03 19:25:30 +0530

    test file added

--- channel_test.go
+++ channel_test.go
@@ -2,35 +2,608 @@ package channels
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
+	"github.com/spurtcms/auth"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var SecretKey = "Secret123"
 
-func TestChannelList(t *testing.T) {
+// Db connection
+func DBSetup() (*gorm.DB, error) {
 
-	channel := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
-	})
-
-	chanlist, count, err := channel.ListChannel(10, 0, Filter{}, true, true)
-
-	if err != nil {
-
-		panic(err)
+	dbConfig := map[string]string{
+		"username": "postgres",
+		"password": "*****",
+		"host":     "localhost",
+		"port":     "5432",
+		"dbname":   "spurtcms",
 	}
 
-	fmt.Println(chanlist, count)
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: "user=" + dbConfig["username"] + " password=" + dbConfig["password"] +
+			" dbname=" + dbConfig["dbname"] + " host=" + dbConfig["host"] +
+			" port=" + dbConfig["port"] + " sslmode=disable TimeZone=Asia/Kolkata",
+	}), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
-func TestGetchannelByName(t *testing.B) {
+// test list channel
+func TestChannelList(t *testing.T) {
 
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     1,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		chanlist, count, err := channel.ListChannel(10, 0, Filter{}, true, true)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(chanlist, count)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
 
-func TestGetChannelById(t *testing.T) {
+// test create channel
+func TestCreateChennal(t *testing.T) {
 
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		chanlist, err := channel.CreateChannel(ChannelCreate{ChannelName: "life style", ChannelDescription: "collections", CategoryIds: []string{"1", "2"}})
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(chanlist, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test create additionalfields
+func TestCreateAdditionalFields(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		field1 := Section{SectionId: 1, SectionName: "date", MasterFieldId: 2, OrderIndex: 1}
+		field2 := Section{SectionId: 2, SectionName: "date&time", OrderIndex: 2}
+
+		optval1 := OptionValues{Value: "1"}
+		optval2 := OptionValues{Value: "2"}
+
+		field1value := Fiedlvalue{Url: "/defaultchannel", OrderIndex: 1, OptionValue: []OptionValues{optval1, optval2}}
+		field2value := Fiedlvalue{Url: "/defaultchannel", OrderIndex: 2}
+
+		err := channel.CreateAdditionalFields(ChannelAddtionalField{Sections: []Section{field1, field2}, FieldValues: []Fiedlvalue{field1value, field2value}}, 5)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test getchannelbyname
+func TestGetchannelByName(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		channel, err := channel.GetchannelByName("life style")
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(channel, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test getchannelbyid
+func TestGetChannelsById(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		channel, chn_categories, err := channel.GetChannelsById(3)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(channel, chn_categories, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test getchannelfieldsbyid
+func TestGetChannelsFieldsById(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		section, fields, err := channel.GetChannelsFieldsById(3)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(section, fields, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test deletechannel
+func TestDeleteChannel(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		err := channel.DeleteChannel(3, 1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test deletechannelpermissions function
+func TestDeleteChannelPermissions(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		err := channel.DeleteChannelPermissions(3)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test chanegechannelstatus function 
+func TestChangeChannelStatus(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		flg,err := channel.ChangeChannelStatus(3,0,1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(flg,err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test getallmasterfieldtype function 
+func TestGetAllMasterFieldType(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		fields,err := channel.GetAllMasterFieldType()
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(fields,err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test editchannel function
+func TestEditChannel(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		err := channel.EditChannel("go","about golang",1,3,[]string{"1", "2"})
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
+}
+
+// test updatechannelfileds function
+func TestUpdateChannelField(t *testing.T) {
+
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
+	})
+
+	if permisison {
+
+		field1 := Section{SectionId: 1, SectionName: "text", MasterFieldId: 2, OrderIndex: 1}
+		field2 := Section{SectionId: 2, SectionName: "date&time", OrderIndex: 2}
+
+		optval1 := OptionValues{Value: "1",FieldId: 7}
+		optval2 := OptionValues{Value: "2",FieldId: 7}
+
+		field1value := Fiedlvalue{Url: "/defaultchannel", OrderIndex: 1, OptionValue: []OptionValues{optval1, optval2},FieldId: 7}
+		field2value := Fiedlvalue{Url: "/defaultchannel", OrderIndex: 2,FieldId: 8}
+
+		err := channel.UpdateChannelField(ChannelUpdate{Sections: []Section{field1},FieldValues: []Fiedlvalue{field1value,field2value},Deletesections: []Section{field2},DeleteFields: []Fiedlvalue{field2value},DeleteOptionsvalue: []OptionValues{optval2},CategoryIds:[]string{"1", "2"},ModifiedBy: 1},3)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
--- entires_test.go
+++ entires_test.go
@@ -2,129 +2,425 @@ package channels
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
-	"gorm.io/gorm"
+	"github.com/spurtcms/auth"
 )
 
+// test channelentrieslist functon
 func TestChannelEntriesList(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     1,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	entries, fitlercount, totolcount, err := chann.ChannelEntriesList(Entries{Limit: 10, Offset: 0, Publishedonly: true})
+	if permisison {
 
-	fmt.Println(entries, fitlercount, totolcount, err)
+		entries, fitlercount, totolcount, err := channel.ChannelEntriesList(Entries{Limit: 10, Offset: 0, Publishedonly: false})
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(entries, fitlercount, totolcount, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 
 }
 
+// test entriesdetailsbyid function
 func TestEntryDetailsById(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	entry, err := chann.EntryDetailsById(IndivEntriesReq{EntryId: 1, MemberProfile: true})
+	if permisison {
 
-	fmt.Println(entry, err)
+		entries, err := channel.EntryDetailsById(IndivEntriesReq{EntryId: 1, MemberProfile: true})
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(entries, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
 
 func TestCreateEntry(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	entry, flg, err := chann.CreateEntry(EntriesRequired{Title: "default", Content: "default"})
+	if permisison {
 
-	fmt.Println(entry, flg, err)
+		entries, flg, err := channel.CreateEntry(EntriesRequired{Title: "java", Content: "about java", ChannelId: 3})
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(entries, flg, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
 
+// test createchannelentryfields function
 func TestCreateChannelEntryFields(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	err := chann.CreateChannelEntryFields(1, 2, []AdditionalFields{
-		{Id: 1, FieldName: "title", FieldValue: "demouser"},
-	})
+	if permisison {
 
-	fmt.Println(err)
+		err := channel.CreateChannelEntryFields(1, 2, []AdditionalFields{
+			{Id: 1, FieldName: "title", FieldValue: "demouser"},
+		})
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 
 }
 
+// test updateentry function
 func TestUpdateEntry(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	flg, err := chann.UpdateEntry(EntriesRequired{Title: "default2", Content: "default2"}, "default", 1)
+	if permisison {
 
-	fmt.Println(flg, err)
+		flg, err := channel.UpdateEntry(EntriesRequired{Title: "default2", Content: "default2"}, "default", 1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(flg, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
 
 func TestUpdateAdditionalField(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	flg, err := chann.UpdateAdditionalField([]AdditionalFields{{Id: 1, FieldName: "title", FieldValue: "sam"}}, 1)
+	if permisison {
 
-	fmt.Println(flg, err)
+		flg, err := channel.UpdateAdditionalField([]AdditionalFields{{Id: 1, FieldName: "title", FieldValue: "sam"}}, 1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(flg, err)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
 
+// test deleteentry function
 func TestDeleteEntry(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	err := chann.DeleteChannel(1, 2)
+	if permisison {
 
-	fmt.Println(err)
+		flg,err := channel.DeleteEntry("Default_Channel",1, 1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(flg)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
 
+// test makefeatureentry function
 func TestMakeFeatureEntry(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	flg, err := chann.MakeFeatureEntry(1, 1, 1)
+	if permisison {
 
-	fmt.Println(flg, err)
+		flg, err := channel.MakeFeatureEntry(1, 1, 1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(flg)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 
 }
 
+// test entrystatus function
 func TestEntryStatus(t *testing.T) {
 
-	chann := ChannelSetup(Config{
-		DB:               &gorm.DB{},
-		AuthEnable:       false,
-		PermissionEnable: false,
+	db, _ := DBSetup()
+
+	config := auth.Config{
+		UserId:     1,
+		ExpiryTime: 2,
+		ExpiryFlg:  true,
+		SecretKey:  "Secret123",
+		DB:         db,
+		RoleId:     2,
+	}
+
+	Auth := auth.AuthSetup(config)
+
+	token, _ := Auth.CreateToken()
+
+	Auth.VerifyToken(token, SecretKey)
+
+	permisison, _ := Auth.IsGranted("Channels", auth.CRUD)
+
+	channel := ChannelSetup(Config{
+		DB:               db,
+		AuthEnable:       true,
+		PermissionEnable: true,
+		Auth:             Auth,
 	})
 
-	flg, err := chann.EntryStatus("default", 1, 1, 2)
+	if permisison {
 
-	fmt.Println(flg, err)
+		flg, err := channel.EntryStatus("default",1, 1, 1)
+
+		if err != nil {
+
+			panic(err)
+		}
+
+		fmt.Println(flg)
+	} else {
+
+		log.Println("permissions enabled not initialised")
+
+	}
 }
